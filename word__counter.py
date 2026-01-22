34839dc38153d5502eb21cb0b1091fc097485a2c
kiranmv002 <mada.kiran@s.amity.edu>
2026-01-22 16:07:48 +0530

    Added word counter mini project

---
+++ word__counter.py
@@ -0,0 +1,11 @@
+sentence = input("Enter a sentence: ")
+
+words = sentence.lower().split()
+word_count = {}
+
+for word in words:
+    word_count[word] = word_count.get(word, 0) + 1
+
+print("\nWord frequency:")
+for word, count in word_count.items():
+    print(word, ":", count)
