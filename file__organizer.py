c83d5b78503541cfce7a9c3279c3f0c0185dfd5e
M V Kiran <mada.kiran@s.amity.edu>
2026-03-11 21:14:45 +0530

    Update file__organizer.py

--- file__organizer.py
+++ file__organizer.py
@@ -1,23 +1,35 @@
-# Simple file organizer
-# Organizes files in a folder based on file extension
+"""
+Simple File Organizer
+
+Organizes files in a folder based on file extension.
+
+Author: Kiran
+"""
 
 import os
 import shutil
 
 folder_path = "files"
 
-for file_name in os.listdir(folder_path):
-    file_path = os.path.join(folder_path, file_name)
+if not os.path.exists(folder_path):
+    print("Folder not found.")
+else:
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
 
-    if os.path.isfile(file_path):
-        extension = file_name.split(".")[-1]
+        if os.path.isfile(file_path):
 
-        new_folder = os.path.join(folder_path, extension)
+            if "." in file_name:
+                extension = file_name.split(".")[-1]
+            else:
+                extension = "others"
 
-        if not os.path.exists(new_folder):
-            os.mkdir(new_folder)
+            new_folder = os.path.join(folder_path, extension)
 
-        shutil.move(file_path, os.path.join(new_folder, file_name))
+            if not os.path.exists(new_folder):
+                os.mkdir(new_folder)
 
+            new_path = os.path.join(new_folder, file_name)
+            shutil.move(file_path, new_path)
 
-print("Files organized successfully.")
+    print("Files organized successfully.")
