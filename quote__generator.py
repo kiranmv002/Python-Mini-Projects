01618cb6a8ebc5ffcce894c84c1ec9d9288a7b4b
M V Kiran <mada.kiran@s.amity.edu>
2026-03-13 22:54:01 +0530

    Update quote__generator.py

--- quote__generator.py
+++ quote__generator.py
@@ -1,5 +1,11 @@
-# Random Quote Generator
-# This program displays a random quote each time it runs.
+"""
+Random Quote Generator
+
+This program displays a random quote.
+The user can generate quotes multiple times.
+
+Author: Kiran
+"""
 
 import random
 
@@ -13,15 +19,24 @@ def show_quote():
         "Learning to code is learning to think."
     ]
 
-    selected_quote = random.choice(quotes)
-    print("\nQuote of the Day:")
-    print(selected_quote)
+    quote = random.choice(quotes)
+    number = quotes.index(quote) + 1
+
+    print("\nQuote", number)
+    print(quote)
 
 
 def main():
     print("Random Quote Generator")
     print("----------------------")
-    show_quote()
+
+    while True:
+        show_quote()
+        again = input("\nDo you want another quote? (y/n): ").lower()
+
+        if again != "y":
+            print("Thanks for using the Quote Generator.")
+            break
 
 
 if __name__ == "__main__":
